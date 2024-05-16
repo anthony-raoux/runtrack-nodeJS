07ca59dc9d1a9a4a30ea140c97a80d73576fe1ed
anthony-raoux <anthony.raoux@laplateforme.io>
2024-05-16 11:00:18 +0200

    job06

--- jour04/index.js
+++ jour04/index.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const readline = require('readline');
 
 mongoose.connect('mongodb://localhost:27017/LaPlateforme');
 
@@ -22,13 +23,18 @@ const studentSchema = new mongoose.Schema({
 // Modèle basé sur le schéma des étudiants
 const Student = mongoose.model('Student', studentSchema);
 
-async function main() {
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Entrez un numéro d\'étudiant : ', async (students_number) => {
   try {
-    // Récupération de tous les étudiants avec leurs cursus
-    const students = await Student.find().populate('year_id');
+    // Récupération des étudiants avec un numéro d'étudiant plus grand que celui saisi
+    const students = await Student.find({ students_number: { $gt: students_number } }).populate('year_id');
 
     // Affichage des résultats dans la console
-    console.log('Liste des étudiants de La Plateforme avec leurs cursus :');
+    console.log(`Liste des étudiants avec un numéro d'étudiant plus grand que ${students_number} :`);
     students.forEach(student => {
       const cursus = student.year_id ? student.year_id.name : 'Cursus inconnu';
       console.log(`- ${student.firstname} ${student.lastname} (${student.students_number}) - Cursus: ${cursus}`);
@@ -37,7 +43,6 @@ async function main() {
     console.error('Une erreur est survenue :', error);
   } finally {
     mongoose.connection.close();
+    rl.close();
   }
-}
-
-main();
+});
