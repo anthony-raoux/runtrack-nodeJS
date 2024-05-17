dee1a20fe8b34b95d747e19537e9326db8c4be13
anthony-raoux <anthony.raoux@laplateforme.io>
2024-05-17 11:13:49 +0200

    job07 fini

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+
 const port = 80;
 
 app.use(express.static(path.join(__dirname, 'public')));
@@ -15,7 +16,7 @@ app.get('/about', (req, res) => {
 });
 
 app.use((req, res, next) => {
-    res.status(404).send('Page non trouvée');
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(port, () => {
