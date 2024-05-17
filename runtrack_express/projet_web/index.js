bc143ee382dd0ffae41203f2510319dacf6d6f71
anthony-raoux <anthony.raoux@laplateforme.io>
2024-05-17 10:29:57 +0200

    job04 fini

--- runtrack_express/projet_web/index.js
+++ runtrack_express/projet_web/index.js
@@ -1,11 +1,21 @@
 const express = require('express');
-
 const app = express();
 
 const port = 3000;
 
+// Route pour la page d'accueil
 app.get('/', (req, res) => {
-    res.send('Bienvenue sur mon site web!');
+    res.send('Bienvenue sur la page d\'accueil de mon site web!');
+});
+
+// Route pour la page "about"
+app.get('/about', (req, res) => {
+    res.send('Bienvenue sur la page "À propos".');
+});
+
+// Middleware pour gérer les erreurs 404
+app.use((req, res, next) => {
+    res.status(404).send('Page non trouvée');
 });
 
 app.listen(port, () => {
