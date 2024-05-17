5d32568170b7871a87748dff3ed34314a1467361
anthony-raoux <anthony.raoux@laplateforme.io>
2024-05-17 10:13:41 +0200

    job02 fini

---
+++ runtrack_express/projet_web/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur mon site web!');
+});
+
+app.listen(port, () => {
+    console.log(`Le serveur fonctionne sur http://localhost:${port}`);
+});
